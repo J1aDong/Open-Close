7619a7c50cd94108c52a7550b0f25819f5e90d65
J1aDong <991383877@qq.com>
2016-11-26 09:49:30 +0800

    添加README.md

---
+++ Open&Close/ColorUtil.swift
@@ -0,0 +1,19 @@
+//
+//  ColorUtil.swift
+//  Open&Close
+//
+//  Created by J1aDong on 2016/11/26.
+//  Copyright © 2016年 J1aDong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorUtil {
+    static func rgbColorByHex(hexColor:Int) -> UIColor{
+        return UIColor(red: ((CGFloat)((hexColor & 0xFF0000) >> 16)) / 255.0,
+                       green: ((CGFloat)((hexColor & 0xFF00) >> 8)) / 255.0,
+                       blue: ((CGFloat)(hexColor & 0xFF)) / 255.0,
+                       alpha: 1.0)
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Open&Close
+>  swift编写，原型来自dribble
+
+![](https://d13yacurqjgara.cloudfront.net/users/107759/screenshots/1623679/menu.gif)
+
+## 使用的第三方库
+* snapkit
+* pop
